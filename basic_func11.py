c86909fc864784dbb1e5f91c79322ed01bfdd25c
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-07-10 18:39:48 +0500

    abs

--- basic_func11.py
+++ basic_func11.py
@@ -1,10 +1,15 @@
-def main(a):
-    '''find the absolute value of a and return it
+# def main(a):
+#     '''find the absolute value of a and return it
     
-    Args:
-        a (int): a number
+#     Args:
+#         a (int): a number
         
-    Returns:
-        int: the absolute value.
-    '''
-    return
+#     Returns:
+#         int: the absolute value.
+#     '''
+#     return
+def main(a):
+    b=(abs(a))
+    return b
+son=main(-8)
+print(son)
