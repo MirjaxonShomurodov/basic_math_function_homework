101938ea776b76066f28b6bbfbb0f3b4bc3686f0
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-07-10 22:19:37 +0500

    round

--- basic_func18.py
+++ basic_func18.py
@@ -1,5 +1,11 @@
+from math import pi
 def main(a):
-    '''Assign the value pi to the parametr "a". Round the result to 2 decimal places and return it.
+    a=(pi)
+    b=round(a,2)
+    return b
+son=(main(3.141592653589793))
+print(son)
+'''Assign the value pi to the parametr "a". Round the result to 2 decimal places and return it.
     
     Args:
         a (float): a number
@@ -7,4 +13,3 @@ def main(a):
     Returns:
         float: the result.
     '''
-    return
